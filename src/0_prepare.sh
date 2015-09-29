54a331a2311e6a46c471be983c1b0321a95db0b4
Jiannan Ouyang <ouyang@cs.pitt.edu>
2015-09-29 15:14:45 -0400

    added dependent centos packages

--- src/0_prepare.sh
+++ src/0_prepare.sh
@@ -6,3 +6,7 @@ mkdir work
 # -p stops errors if the directory already exists
 mkdir -p source
 
+# dependent packages for centos
+sudo yum groupinstall "Development Tools"
+sudo yum install net-tools kvm qemu-kvm wget unzip git vim bc ncurses ncurses-devel glibc-static syslinux genisoimage
+
